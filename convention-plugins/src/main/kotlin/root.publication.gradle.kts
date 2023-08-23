435e83bfe24c352c39dcc762683566e434865cf6
M0Coding <mohamedrejeb445@gmail.com>
2023-08-23 10:09:02 +0100

    Update publish config

--- convention-plugins/src/main/kotlin/root.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/root.publication.gradle.kts
@@ -12,7 +12,7 @@ nexusPublishing {
     // https://github.com/gradle-nexus/publish-plugin#publishing-to-maven-central-via-sonatype-ossrh
     repositories {
         sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
             stagingProfileId.set(System.getenv("OSSRH_STAGING_PROFILE_ID"))
             username.set(System.getenv("OSSRH_USERNAME"))
