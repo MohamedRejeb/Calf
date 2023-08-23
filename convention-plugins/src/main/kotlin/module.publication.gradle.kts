119a364cf07b4e6a5cc1d622ff7313d4331a2d04
M0Coding <mohamedrejeb445@gmail.com>
2023-08-23 10:29:22 +0100

    Update publish config

--- convention-plugins/src/main/kotlin/module.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/module.publication.gradle.kts
@@ -53,3 +53,8 @@ signing {
         sign(publishing.publications)
     }
 }
+
+// TODO: remove after https://youtrack.jetbrains.com/issue/KT-46466 is fixed
+project.tasks.withType(AbstractPublishToMaven::class.java).configureEach {
+    dependsOn(project.tasks.withType(Sign::class.java))
+}
