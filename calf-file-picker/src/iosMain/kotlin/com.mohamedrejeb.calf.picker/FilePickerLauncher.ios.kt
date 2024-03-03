f7e0d042a846958e439680d84507c4b3d8d09d92
MohamedRejeb <mohamedrejeb445@gmail.com>
2024-03-03 18:12:35 +0100

    Fix build error

--- calf-file-picker/src/iosMain/kotlin/com.mohamedrejeb.calf.picker/FilePickerLauncher.ios.kt
+++ calf-file-picker/src/iosMain/kotlin/com.mohamedrejeb.calf.picker/FilePickerLauncher.ios.kt
@@ -8,7 +8,6 @@ import kotlinx.cinterop.BetaInteropApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import platform.Foundation.NSData
 import platform.Foundation.NSURL
 import platform.Photos.PHPhotoLibrary
 import platform.PhotosUI.PHPickerConfiguration
@@ -51,48 +50,51 @@ private fun rememberDocumentPickerLauncher(
     onResult: (List<KmpFile>) -> Unit,
 ): FilePickerLauncher {
     val coroutineScope = rememberCoroutineScope()
-    val delegate = remember {
-        object : NSObject(), UIDocumentPickerDelegateProtocol {
-            override fun documentPicker(
-                controller: UIDocumentPickerViewController,
-                didPickDocumentAtURL: NSURL
-            ) {
-                onResult(listOf(didPickDocumentAtURL))
-            }
-
-            override fun documentPicker(
-                controller: UIDocumentPickerViewController,
-                didPickDocumentsAtURLs: List<*>
-            ) {
-                val dataList = didPickDocumentsAtURLs.mapNotNull {
-                    it as? NSURL
+    val delegate =
+        remember {
+            object : NSObject(), UIDocumentPickerDelegateProtocol {
+                override fun documentPicker(
+                    controller: UIDocumentPickerViewController,
+                    didPickDocumentAtURL: NSURL,
+                ) {
+                    onResult(listOf(KmpFile(didPickDocumentAtURL)))
                 }
-                coroutineScope.launch {
-                    withContext(Dispatchers.Main) {
-                        onResult(dataList)
+
+                override fun documentPicker(
+                    controller: UIDocumentPickerViewController,
+                    didPickDocumentsAtURLs: List<*>,
+                ) {
+                    val dataList =
+                        didPickDocumentsAtURLs.mapNotNull {
+                            (it as? NSURL)?.let(::KmpFile)
+                        }
+                    coroutineScope.launch {
+                        withContext(Dispatchers.Main) {
+                            onResult(dataList)
+                        }
                     }
                 }
             }
         }
-    }
 
     return remember {
         FilePickerLauncher(
             type = type,
             selectionMode = selectionMode,
             onLaunch = {
-                val pickerController = createUIDocumentPickerViewController(
-                    delegate = delegate,
-                    type = type,
-                    selectionMode = selectionMode,
-                )
+                val pickerController =
+                    createUIDocumentPickerViewController(
+                        delegate = delegate,
+                        type = type,
+                        selectionMode = selectionMode,
+                    )
 
                 UIApplication.sharedApplication.keyWindow?.rootViewController?.presentViewController(
                     pickerController,
                     true,
-                    null
+                    null,
                 )
-            }
+            },
         )
     }
 }
@@ -103,42 +105,47 @@ private fun rememberImagePickerLauncher(
     selectionMode: FilePickerSelectionMode,
     onResult: (List<KmpFile>) -> Unit,
 ): FilePickerLauncher {
-    val pickerDelegate = remember {
-        object : NSObject(), PHPickerViewControllerDelegateProtocol {
-            override fun picker(picker: PHPickerViewController, didFinishPicking: List<*>) {
-                picker.dismissViewControllerAnimated(true, null)
-                println("didFinishPicking: $didFinishPicking")
-                didFinishPicking.forEach {
-                    val result = it as? PHPickerResult ?: return@forEach
-                    result.itemProvider.loadFileRepresentationForTypeIdentifier(
-                        typeIdentifier = UTTypeImage.identifier,
-                    ) { url, error ->
-                        if (error != null) {
-                            println("Error: $error")
-                            return@loadFileRepresentationForTypeIdentifier
+    val pickerDelegate =
+        remember {
+            object : NSObject(), PHPickerViewControllerDelegateProtocol {
+                override fun picker(
+                    picker: PHPickerViewController,
+                    didFinishPicking: List<*>,
+                ) {
+                    picker.dismissViewControllerAnimated(true, null)
+                    println("didFinishPicking: $didFinishPicking")
+                    didFinishPicking.forEach {
+                        val result = it as? PHPickerResult ?: return@forEach
+                        result.itemProvider.loadFileRepresentationForTypeIdentifier(
+                            typeIdentifier = UTTypeImage.identifier,
+                        ) { url, error ->
+                            if (error != null) {
+                                println("Error: $error")
+                                return@loadFileRepresentationForTypeIdentifier
+                            }
+                            onResult(listOfNotNull(url?.let(::KmpFile)))
                         }
-                        onResult(listOfNotNull(url))
                     }
                 }
             }
         }
-    }
 
     return remember {
         FilePickerLauncher(
             type = type,
             selectionMode = selectionMode,
             onLaunch = {
-                val imagePicker = createPHPickerViewController(
-                    delegate = pickerDelegate,
-                    selectionMode = selectionMode,
-                )
+                val imagePicker =
+                    createPHPickerViewController(
+                        delegate = pickerDelegate,
+                        selectionMode = selectionMode,
+                    )
                 UIApplication.sharedApplication.keyWindow?.rootViewController?.presentViewController(
                     imagePicker,
                     true,
-                    null
+                    null,
                 )
-            }
+            },
         )
     }
 }
@@ -148,23 +155,25 @@ private fun createUIDocumentPickerViewController(
     type: FilePickerFileType,
     selectionMode: FilePickerSelectionMode,
 ): UIDocumentPickerViewController {
-    val contentTypes = type.value.mapNotNull { mimeType ->
-        when (mimeType) {
-            FilePickerFileType.ImageContentType -> UTTypeImage
-            FilePickerFileType.VideoContentType -> UTTypeVideo
-            FilePickerFileType.AudioContentType -> UTTypeAudio
-            FilePickerFileType.DocumentContentType -> UTTypeApplication
-            FilePickerFileType.TextContentType -> UTTypeText
-            FilePickerFileType.AllContentType -> UTTypeData
-            FilePickerFileType.FolderContentType -> UTTypeFolder
-            else -> UTType.typeWithMIMEType(mimeType)
+    val contentTypes =
+        type.value.mapNotNull { mimeType ->
+            when (mimeType) {
+                FilePickerFileType.ImageContentType -> UTTypeImage
+                FilePickerFileType.VideoContentType -> UTTypeVideo
+                FilePickerFileType.AudioContentType -> UTTypeAudio
+                FilePickerFileType.DocumentContentType -> UTTypeApplication
+                FilePickerFileType.TextContentType -> UTTypeText
+                FilePickerFileType.AllContentType -> UTTypeData
+                FilePickerFileType.FolderContentType -> UTTypeFolder
+                else -> UTType.typeWithMIMEType(mimeType)
+            }
         }
-    }
 
-    val pickerController = UIDocumentPickerViewController(
-        forOpeningContentTypes = contentTypes,
-        asCopy = type != FilePickerFileType.Folder,
-    )
+    val pickerController =
+        UIDocumentPickerViewController(
+            forOpeningContentTypes = contentTypes,
+            asCopy = type != FilePickerFileType.Folder,
+        )
     pickerController.delegate = delegate
     pickerController.allowsMultipleSelection = selectionMode == FilePickerSelectionMode.Multiple
     return pickerController
@@ -175,11 +184,12 @@ private fun createPHPickerViewController(
     selectionMode: FilePickerSelectionMode,
 ): PHPickerViewController {
     val configuration = PHPickerConfiguration(PHPhotoLibrary.sharedPhotoLibrary())
-    val newFilter = PHPickerFilter.anyFilterMatchingSubfilters(
-        listOf(
-            PHPickerFilter.imagesFilter(),
+    val newFilter =
+        PHPickerFilter.anyFilterMatchingSubfilters(
+            listOf(
+                PHPickerFilter.imagesFilter(),
+            ),
         )
-    )
     configuration.filter = newFilter
     configuration.preferredAssetRepresentationMode =
         PHPickerConfigurationAssetRepresentationModeCurrent
