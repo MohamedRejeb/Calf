0f3a073ee03d7a13575f8db668e9d9c87712b34b
MohamedRejeb <mohamedrejeb445@gmail.com>
2024-03-03 18:01:26 +0100

    Fix build error

--- calf-sf-symbols/src/commonMain/kotlin/com.mohamedrejeb.calf.sf.symbols/SFSymbols.kt
+++ calf-sf-symbols/src/commonMain/kotlin/com.mohamedrejeb.calf.sf.symbols/SFSymbols.kt
@@ -1,8 +1,3 @@
 package com.mohamedrejeb.calf.sf.symbols
 
-import androidx.compose.runtime.Composable
-import org.jetbrains.compose.resources.painterResource
-
-object SFSymbols {
-    val squareAndArrowUp @Composable get() = painterResource("test.xml")
-}
+object SFSymbols
