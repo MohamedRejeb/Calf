d6a63fb4a1c575a36cc289dd58a2d8a52d01123c
MohamedRejeb <mohamedrejeb445@gmail.com>
2024-07-09 19:10:52 +0100

    Add publish snapshot to sonatype

--- convention-plugins/src/main/kotlin/Publication.kt
+++ convention-plugins/src/main/kotlin/Publication.kt
@@ -12,7 +12,7 @@ fun Project.rootPublicationSetup() {
 
     allprojects {
         group = "com.mohamedrejeb.calf"
-        version = "0.5.1"
+        version = System.getenv("VERSION") ?: "0.5.1"
     }
 
     nexusPublishing {
