5eac9b942270cceebda14a60e828096dc9d1cb67
MohamedRejeb <mohamedrejeb445@gmail.com>
2026-03-16 13:56:50 +0100

    Fix iOS run issue due to having two classes with the same name under the same package

--- calf-permissions/microphone/src/iosMain/kotlin/com.mohamedrejeb.calf/permissions/MicrophonePermissions.ios.kt
+++ calf-permissions/microphone/src/iosMain/kotlin/com.mohamedrejeb.calf/permissions/MicrophonePermissions.ios.kt
@@ -1,11 +1,11 @@
 package com.mohamedrejeb.calf.permissions
 
-import com.mohamedrejeb.calf.permissions.helper.AVCapturePermissionHelper
+import com.mohamedrejeb.calf.permissions.helper.AVMicrophonePermissionHelper
 import com.mohamedrejeb.calf.permissions.helper.PermissionDelegateRegistry
 import platform.AVFoundation.AVMediaTypeAudio
 
 internal actual fun registerRecordAudioPermission(permission: Permission) {
     PermissionDelegateRegistry.register(permission) {
-        AVCapturePermissionHelper(AVMediaTypeAudio)
+        AVMicrophonePermissionHelper(AVMediaTypeAudio)
     }
 }
